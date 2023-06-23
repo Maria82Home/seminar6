df009e8fb3d05484dd89e9f4b08533d67abfb918
Maria <home82@list.ru>
2023-06-23 22:30:07 +0500

    homework6 commit

---
+++ src/main/java/seminar6.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+
+
+public class seminar6 {
+    public static void main(String[] args) {
+        MySet<Integer> ms = new MySet<>();
+        HashSet hs = new HashSet();
+//        hs.add(888);
+        System.out.println(ms.add(456));
+        System.out.println(ms.add(456));
+//        System.out.println(ms.add(4));
+        System.out.println(ms.size());
+        System.out.println(ms.remove(456));
+        System.out.println(ms.remove(456));
+        System.out.println(ms.add(9));
+        System.out.println(ms.add(10));
+
+        Iterator<Integer> iter = ms.iterator();
+                while(iter.hasNext()){
+                    System.out.println(iter.next());
+                }
+    }
+}
+class MySet<E> {
+    private HashMap<E, Object> mp = new HashMap<>();
+    private static final Object OBJ = new Object();
+
+    public boolean add(E num) {
+        return mp.put(num, OBJ) == null;
+    }
+
+    public boolean remove(E num) {
+        return mp.remove(num) != null;
+    }
+
+    public int size() {
+        return mp.size();
+    }
+
+    public Iterator<E> iterator(){
+        return mp.keySet().iterator();
+    }
+
+}
